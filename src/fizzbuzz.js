e2c7b6cdbede5903cf06f99e2c70cf73de288168
Felix <felix.mathiasson99@gmail.com>
2024-09-20 16:27:03 +0200

    fizzbuzz amigo

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,19 @@
 const answer = []
 
 // Write your code below this line
-
+for(let number = 1; number < 16; number++) {
+    let word = ''
+    if(number % 3 === 0) {
+        word = 'Fizz'
+    } 
+    if (number % 5 === 0) {
+        word += 'Buzz'
+    }
+    if(word === '') {
+        word = number
+    }
+    answer.push(word)
+}
 
 
 
